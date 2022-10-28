84a041d55436d525717397354bd78fb2a819351f
Adam Shannon <adamkshannon@gmail.com>
2022-10-28 09:33:08 -0500

    docs: flush out cli help page

--- doc.go
+++ doc.go
@@ -19,10 +19,51 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func help() {
-	fmt.Printf("csvq %s\n", Version)
+	fmt.Printf(strings.TrimSpace(fmt.Sprintf(`
+csvq is a tool for parsing and transforming comma separated values (CSV) files. This tool allows
+for flexible transforms of data files.
 
-	fmt.Println("TODO: write docs")
+COMMAND LINE
+
+   -d <rune>             Delimiter used to separate records. (Default: ',')
+   -keep <string-list>   Column headers to keep in output. Order of kept headers is maintained in output.
+
+   -headers              // TODO(adam):
+
+   -format <string>      Layout to print resulting records in. (Default: csv, Options: csv, tabs, table)
+
+   -v                    Enable verbose logging
+   -version              Print the version of csvq. (Example: %s)
+
+EXAMPLES
+
+Extract first_name and last_name columns (in that order). Sort results.
+   csvq -keep first_name,last_name ~/Downloads/report.csv | sort -u
+
+Change delimiter used in report.csv.
+   csvq -d';' user_id,dob,email ~/Downloads/report.csv
+
+Output CSV columns in a table.
+   csvq -keep first_name,last_name -format table
+
+Combine multiple files.
+   csvq -keep user_id,email ~/Downloads/report1.csv ~/Downloads/report2.csv
+`, Version)))
+
+	// TODO(adam): docs for additional flags
+	// // flagIgnoreCols = flag.String("ignore", "", "Column headers to remove from output") // TODO(adam):
+	// // flagIndices    = flag.String("i", "", "Indicies to keep in output")
+	// // flagNotIndices = flag.String("I", "", "Indicies to remove from output")
+
+	fmt.Println("")
+
+	// // TODO(adam): flag to sort output (useful w/ '-format table')
+	// // IDEA: -sort.asc/-sort.desc 1 (index) or last_name (col name)
+
+	// // TODO(adam): -exec flag to call external program on PATH
+	// //  could we format command with $1, $2 or $first_name, $last_name values?
 }
